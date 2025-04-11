db09f462bce5973027178973aa2187477c49a908
wanny <wankims@gmail.com>
2025-04-11 13:57:23 +0900

    [feature] page 37

--- prime_factors.cpp
+++ prime_factors.cpp
@@ -2,8 +2,11 @@
 
 class PrimeFactor {
 public:
-	std::vector<int> of(int i) {
+	std::vector<int> of(int number) {
 		std::vector<int> result = {};
+		if (number == 2) {
+			result.push_back(2);
+		}
 		return result;
 	}
 };
--- prime_factors_test.cpp
+++ prime_factors_test.cpp
@@ -5,10 +5,16 @@
 
 using namespace std;
 
-TEST(PrimeFactors, PrimeTest) {
+TEST(PrimeFactors, Of1) {
 	PrimeFactor prime_factor;
 	vector<int> expected = {};
 	EXPECT_EQ(expected, prime_factor.of(1));
+}
+
+TEST(PrimeFactors, Of2) {
+	PrimeFactor prime_factor;
+	vector<int> expected = {2};
+	EXPECT_EQ(expected, prime_factor.of(2));
 }
 
 int main()
