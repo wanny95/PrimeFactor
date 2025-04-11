da79d15fa66fae9e42e63908a8157b4eabff35ff
wanny <wankims@gmail.com>
2025-04-11 14:03:10 +0900

    [refactoring] page 42

--- prime_factors.cpp
+++ prime_factors.cpp
@@ -4,11 +4,8 @@ class PrimeFactor {
 public:
 	std::vector<int> of(int number) {
 		std::vector<int> result = {};
-		if (number == 2) {
-			result.push_back(2);
-		}
-		if (number == 3) {
-			result.push_back(3);
+		if (number > 1) {
+			result.push_back(number);
 		}
 		return result;
 	}
