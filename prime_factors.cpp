86cf008204f9c38c9a0b832a38fc800cc77b8e41
wanny <wankims@gmail.com>
2025-04-11 14:19:36 +0900

    [refactoring] page 59

--- prime_factors.cpp
+++ prime_factors.cpp
@@ -4,26 +4,14 @@ class PrimeFactor {
 public:
 	std::vector<int> of(int number) {
 		std::vector<int> result = {};
-		if (number > 1) {
-			int divisor = 2;
-			if (number == 4) {
-				while (number % divisor == 0) {
-					result.push_back(divisor);
-					number /= divisor;
-				}
-			}
-			else if (number == 4 || number == 6 || number == 9 || number == 12) {
-				for (divisor = 2; number > 1; divisor++) {
-					while (number % divisor == 0) {
-						result.push_back(divisor);
-						number /= divisor;
-					}
-				}
-			}
-			else {
-				result.push_back(number);
+
+		for (int divisor = 2; number > 1; divisor++) {
+			while (number % divisor == 0) {
+				result.push_back(divisor);
+				number /= divisor;
 			}
 		}
+
 		return result;
 	}
 };
